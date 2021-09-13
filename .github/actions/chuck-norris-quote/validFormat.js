722c2bfff8a78c95e102d9a9b23623cd602ebc9c
Paul Glavich <glav@theglavs.com>
2021-09-13 10:31:49 +1000

    Changing quote source as previous source was very low quality

--- .github/actions/chuck-norris-quote/validFormat.js
+++ .github/actions/chuck-norris-quote/validFormat.js
@@ -1,11 +1,9 @@
 const FMT_TEXT = 'text';
 const FMT_HTML = 'html';
-const FMT_MARKDOWN = 'markdown';
-const validFormats = [FMT_HTML, FMT_MARKDOWN, FMT_TEXT];
+const validFormats = [FMT_HTML, FMT_TEXT];
 const formatType = {
     Text: FMT_TEXT,
-    Html: FMT_HTML,
-    Markdown: FMT_MARKDOWN
+    Html: FMT_HTML
 }
 
 const quote = {
@@ -19,9 +17,6 @@ const quote = {
     isType: function(quoteFormat) {
         if (quoteFormat === FMT_TEXT) {
             return formatType.Text;
-        }
-        if (quoteFormat === FMT_MARKDOWN) {
-            return formatType.Markdown;
         }
         if (quoteFormat === FMT_HTML) {
             return formatType.Html;
